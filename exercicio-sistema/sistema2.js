b5ead8ed121701efaea435537e3b375c5587b43d
luisa <luisaeduarda.neves@gmail.com>
2026-08-10 11:08:49 -0300

    Add files via upload

---
+++ exercicio-sistema/sistema2.js
@@ -0,0 +1,8 @@
+const os = require('os');
+console.log('===INFORMAÇÕES DO SISTEMA ===');
+console.log('');
+console.log('Plataforma:', process.platform);
+console.log('Arquitetura:', process.arch);
+console.log('Versão do Node.js:', process.version);
+console.log('Diretório atual:', process.cwd());
+console.log('Análise do sistema concluída!');
